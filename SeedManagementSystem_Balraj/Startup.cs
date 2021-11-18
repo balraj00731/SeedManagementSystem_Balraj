1a4cf54e232e35f5c0ceaedb094d5121476efea9
Balraj00731@gmail.com <Balraj00731@gmail.com>
2021-11-18 15:43:18 +0300

    Add project files.

---
+++ SeedManagementSystem_Balraj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeedManagementSystem_Balraj.Startup))]
+namespace SeedManagementSystem_Balraj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
